af1b7bb1a5f94c52eb5a134219e4dd038ea83fa8
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-02-23 22:15:11 +0300

    facing a problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["dist/output.css{html,js}"],
+  content: ["{html,js}", "index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
